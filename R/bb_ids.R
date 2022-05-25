a87314a08c09e8cd6b312b25c961234b7502c3b2
modche <michaelstoelzle@me.com>
2022-05-25 21:47:19 +0200

    initial realease

--- R/bb_ids.R
+++ R/bb_ids.R
@@ -1,3 +1,16 @@
+
+#' Function to extract ID (polygon_id) for one or multiple gauging stations.
+#'
+#' @param stations data.frame with gauging station with x, y
+#' @param shape path to main shapefile
+#' @param debug boolean; prints working process
+#' @param polygon_col name of id column in shapefile
+#'
+#' @return ID of basin that includes the gauging station.
+#' @details Stations must be a data.frame of one or more gauging stations with x and y (longitude, latitude in WGS84) columns.
+#' @export
+#'
+#' @examples
 bb_ids <- function(stations, 
                    shape, 
                    debug = FALSE, 
@@ -12,9 +25,9 @@ bb_ids <- function(stations,
     for (i in 1:nrow(stations)) {
         if(debug) cat(i," ")
         
-        bb_shape <- shape %>% filter(x[i] > xmin, x[i] < xmax, y[i] > ymin, y[i] < ymax) 
-        extract_id <- st_join(station_sf[i,], bb_shape, join = st_within) %>% 
-            pull({polygon_col})
+        bb_shape <- shape %>% dplyr::filter(x[i] > xmin, x[i] < xmax, y[i] > ymin, y[i] < ymax) 
+        extract_id <- st_join(station_sf[i,], bb_shape, join = sf::st_within) %>% 
+            dplyr::pull({polygon_col})
         
         if(length(extract_id) != 1) message('Station has more than 1 matching polygon. Using first.')
         ids[i] <- extract_id[1]
